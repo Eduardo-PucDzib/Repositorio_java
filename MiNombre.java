085f15b9795e25e55bb477141eecb2c258a2e180
urieluc <8560@itescam.edu.mx>
2024-03-01 15:16:19 -0600

    se añadio en archivo.java mi nombre

--- MiNombre.java
+++ MiNombre.java
@@ -1,5 +1,7 @@
 public class MiNombre{
 	public static void main (String args[]){
 		System.out.println("Jose Eduardo Puc Dzib_8346")
+		system.out.println("\n")
+		System.out.println("Edgar uriel uc tun")
 	}
 }
