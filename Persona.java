054eb0b8730950bf98d3d5702432ba898f7b5682
Administracion-Organizacion-de-Datos_8346 <8346@itescam.edu.mx>
2024-03-01 15:42:13 -0600

    Se creo el archivo.java con clase persona y agregue mi nombre con mi matricula

---
+++ Persona.java
@@ -0,0 +1,40 @@
+public class Persona {
+
+    private String nombre;
+    private String matricula;
+
+    public Persona(String nombre, String matricula) {
+        this.nombre = nombre;
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public static void main(String[] args) {
+
+        Persona persona1 = new Persona("JOse Eduardo Puc Dzib", "8346");
+
+        System.out.println("Alumno Itescam 1:");
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Matrícula: " + persona1.getMatricula());
+        System.out.println();
+
+    }
+
+}
+
+
