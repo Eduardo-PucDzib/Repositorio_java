982b6bc972241092c8099ce78d1eecfc1d6a2c9e
urieluc <8560@itescam.edu.mx>
2024-03-01 15:56:07 -0600

    se actualizo la clase .java com mi nombre y mi matricula

--- Persona.java
+++ Persona.java
@@ -27,11 +27,17 @@ public class Persona {
     public static void main(String[] args) {
 
         Persona persona1 = new Persona("JOse Eduardo Puc Dzib", "8346");
+	Persona persona2 = new Persona("Edgar uriel uc tun", "8560");
 
         System.out.println("Alumno Itescam 1:");
         System.out.println("Nombre: " + persona1.getNombre());
         System.out.println("Matrícula: " + persona1.getMatricula());
         System.out.println();
+	
+	System.out.println("Alumno itescam 2");
+	System.out.println("Nombre: " + persona2.getNombre());
+	System.out.println("Matrícula: " + persona2.getMatricula());
+	System.out.println();
 
     }
 
